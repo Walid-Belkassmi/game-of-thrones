459119a6e3af83b9e753c869070cdccb49bf5aa0
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-20 17:28:27 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Character from './components/Character'
-import "./App.css"
+import React from "react";
+import Character from "./components/Character";
+import "./App.css";
 
 class App extends React.Component {
-  constructor (){
-    super()
+  constructor() {
+    super();
     this.state = {
       characters: [],
       favorites: [],
@@ -12,136 +12,152 @@ class App extends React.Component {
       showCharacters: true,
       showFavorites: false,
       showContinents: false,
-    }
+    };
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     // personnages de got
-    const request = await fetch("https://thronesapi.com/api/v2/Characters")
-    const response = await request.json()
-    this.setState({
-      characters: response,
-    })
+    const request = await fetch("https://thronesapi.com/api/v2/Characters");
+    const response = await request.json();
 
     // continents
-    const requestContinents = await fetch("https://thronesapi.com/api/v2/Continents")
-    const responseContinents = await requestContinents.json()
-    console.log(responseContinents)
-    
+    const requestContinents = await fetch(
+      "https://thronesapi.com/api/v2/Continents"
+    );
+    const responseContinents = await requestContinents.json();
+
     this.setState({
+      characters: response,
       continents: responseContinents,
-    })
+    });
   }
 
   handleFavoriteClick = (character) => {
-    const clonedFavorites = [ ...this.state.favorites, character]
+    const clonedFavorites = [...this.state.favorites, character];
 
     this.setState({
       favorites: clonedFavorites,
-    })
-  }
+    });
+  };
 
   handleRemoveFavoriteClick = (character) => {
-    const clonedFavorites = [...this.state.favorites]
-    const index = clonedFavorites.indexOf(character)
+    const clonedFavorites = [...this.state.favorites];
+    const index = clonedFavorites.indexOf(character);
     clonedFavorites.splice(index, 1);
 
     this.setState({
-        favorites: clonedFavorites,
-    })
-  }
+      favorites: clonedFavorites,
+    });
+  };
 
   handleCharactersClick = () => {
     this.setState({
       showCharacters: true,
       showFavorites: false,
       showContinents: false,
-    })
-  }
+    });
+  };
 
   handleFavoriteSection = () => {
     this.setState({
       showCharacters: false,
       showFavorites: true,
-      showContinents: false
-    })
-  }
+      showContinents: false,
+    });
+  };
 
   handleContinentsClick = () => {
     this.setState({
       showCharacters: false,
       showFavorites: false,
-      showContinents: true
-    })
-  }
+      showContinents: true,
+    });
+  };
 
-	render() {
-		return(
-      <div className='Container'>
-        <h1 className='text-center mt-3 text-light'>Game of thrones</h1>
+  render() {
+    return (
+      <div className="Container">
+        <h1 className="text-center mt-3 text-light">Game of thrones</h1>
         <div className="onglets d-flex justify-content-center mt-5 mb-3 gap-3">
-          <button onClick={this.handleCharactersClick} type="button" class="btn btn-outline-light">Characters</button>
-          <button  onClick={this.handleFavoriteSection} type="button" class="btn btn-outline-light">Favorites</button>
-          <button  onClick={this.handleContinentsClick} type="button" class="btn btn-outline-light">Continents</button>
+          <button
+            onClick={this.handleCharactersClick}
+            type="button"
+            className="btn btn-outline-light"
+          >
+            Characters
+          </button>
+          <button
+            onClick={this.handleFavoriteSection}
+            type="button"
+            className="btn btn-outline-light"
+          >
+            Favorites
+          </button>
+          <button
+            onClick={this.handleContinentsClick}
+            type="button"
+            className="btn btn-outline-light"
+          >
+            Continents
+          </button>
         </div>
-        <div className='d-flex flex-wrap justify-content-center'>
-          {this.state.showCharacters && 
-          <>
-          {this.state.characters.map((character) => {
-            return(
-              <Character
-                name = {character.fullName}
-                title = {character.title}
-                image= {character.imageUrl}
-                favorite = {() => this.handleFavoriteClick(character)}
-              />
-            )
-          })}
-          </>
-         }
+        <div className="d-flex flex-wrap justify-content-center">
+          {this.state.showCharacters && (
+            <>
+              {this.state.characters.map((character) => {
+                return (
+                  <Character
+                    key={`${character.fullName}${character.id}`}
+                    name={character.fullName}
+                    title={character.title}
+                    image={character.imageUrl}
+                    favorite={() => this.handleFavoriteClick(character)}
+                  />
+                );
+              })}
+            </>
+          )}
         </div>
 
         <ul className="p-3">
-            {this.state.showFavorites && 
+          {this.state.showFavorites && (
             <>
-            <h3 className='text-warning'>Favorites :</h3>
-            {this.state.favorites.map((character) => {
-              return (
-                <li className="text-dark d-flex justify-content-between m-2">
-                  {/* {character.fullName} */}
-                  <Character
-                    name = {character.fullName}
-                    title = {character.title}
-                    image= {character.imageUrl}
-                  />
-                  <button
-                   className="ms-2 btn btn-danger"
-                   onClick={() => this.handleRemoveFavoriteClick(character)}
-                  >
-                  X
-                  </button>
-                </li>
-              );
-            })}
+              <h3 className="text-warning">Favorites :</h3>
+              {this.state.favorites.map((favorite) => {
+                return (
+                  <li className="text-dark d-flex justify-content-between m-2">
+                    {/* {favorite.fullName} */}
+                    <Character
+                      name={favorite.fullName}
+                      title={favorite.title}
+                      image={favorite.imageUrl}
+                    />
+                    <button
+                      className="ms-2 btn btn-danger"
+                      onClick={() => this.handleRemoveFavoriteClick(favorite)}
+                    >
+                      X
+                    </button>
+                  </li>
+                );
+              })}
             </>
-            }
+          )}
         </ul>
 
-        <section className='continents'>
-          {this.state.showContinents && 
+        <section className="continents">
+          {this.state.showContinents && (
             <>
-            <h3>Continents</h3>
-            {this.state.continents.map((continent) => {
-              return (
-                <p>{continent.name}</p>
-              )
-            })}
-          </>
-          }
+              <h3>Continents</h3>
+              {this.state.continents.map((continent) => {
+                return <p>{continent.name}</p>;
+              })}
+            </>
+          )}
         </section>
       </div>
-		)
-	}
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
 class Character extends React.Component {
-	render() {
-		return(
-            <div className='card col-3 m-4 p-4'>
-                <img src={this.props.image} alt={this.props.name} />
-                <h2 className='mt-2 text-center'>{this.props.name}</h2>
-                <h3 className='fst-italic text-center mt-2'>{this.props.title}</h3>
-                <button onClick={this.props.favorite} type="button" class="btn btn-dark">Add to favorite</button>
-            </div>
-		)
-	}
+  render() {
+    return (
+      <div className="card col-3 m-4 p-4">
+        <img src={this.props.image} alt={this.props.name} />
+        <h2 className="mt-2 text-center">{this.props.name}</h2>
+        <h3 className="fst-italic text-center mt-2">{this.props.title}</h3>
+        {this.props.favorite && (
+          <button
+            onClick={this.props.favorite}
+            type="button"
+            className="btn btn-dark"
+          >
+            Add to favorite
+          </button>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Character
+export default Character;
